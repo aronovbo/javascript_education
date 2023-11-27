265e0c4b1fc6d1f07bcb124dbddb2771f29b7da1
aronovbo <aronovbo@gmail.com>
2023-11-28 00:36:40 +0300

    16 exercise

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello world !')
+// console.log('Hello world !')
+const payRateUSD = 80;
+const hours = 5 * (11 - 2);
+const workHours = 40;
+console.log('Смогу ли я работать? ' + (hours > workHours));
+console.log('Сколько я заработаю? ' + (workHours * payRateUSD) + ' USD');
